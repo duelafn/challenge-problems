d2d601c606d2f02aff31c6c9cb4cfe93b53f689b
Dean Serenevy <dean@serenevy.net>
2016-12-03 08:03:54 -0500

    Add somewhat golfed versions

--- 03.pl
+++ 03.pl
@@ -10,6 +10,11 @@ multi MAIN('rows') {
     say $possible;
 }
 
+multi MAIN('golf') {
+    say "rows: ", [+] "03.in".IO.lines.map: { my @n = .comb(/\d+/)».Int.sort; @n[0] + @n[1] > @n[2] };
+    say "cols: ", [+] flat([Z] "03.in".IO.lines».comb(/\d+/)».Int).rotor(3)».sort.map: { .[0] + .[1] > .[2] };
+}
+
 multi MAIN('cols') {
     my $possible = 0;
     for flat([Z] "03.in".IO.lines».comb(/\d+/)».Int) -> $a, $b, $c {
