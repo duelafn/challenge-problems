8da6412ef1541a1815df288d7e69a17085455644
Dean Serenevy <dean@serenevy.net>
2016-12-07 09:31:39 -0500

    Add some comments to Day 3 in response to a request for explanation

--- 03.pl
+++ 03.pl
@@ -1,25 +1,41 @@
 #!/usr/bin/env perl6
 use v6.c;
 
-multi MAIN('rows') {
+multi MAIN('rows', $file="03.in") {
     my $possible = 0;
-    for "03.in".IO.lines -> $line {
+    for $file.IO.lines -> $line {
         my @num = sort $line.comb(/\d+/)».Int;
         $possible++ if @num[0] + @num[1] > @num[2];
     }
     say $possible;
 }
 
-multi MAIN('golf') {
-    say "rows: ", [+] "03.in".IO.lines.map:{my \n=.comb(/\d+/)».Int.sort;n[0]+n[1]>n[2]};
-    say "cols: ", [+] flat([Z] "03.in".IO.lines».comb(/\d+/)».Int).rotor(3)».sort.map:{.[0]+.[1]>.[2]};
-}
-
-multi MAIN('cols') {
+multi MAIN('cols', $file="03.in") {
     my $possible = 0;
-    for flat([Z] "03.in".IO.lines».comb(/\d+/)».Int) -> $a, $b, $c {
+    # $file.IO.lines           # read in file lines [ line1, line2, ... ]
+    #    ».comb(/\d+/)         # for each line, produce a list from sequences of digits ( ("1", "2", "3"), ("4", "5", "6"), ... )
+    #    ».Int                 # convert to integers so the sort works later ( (1, 2, 3), (4, 5, 6), ... )
+    #                          # Note: ». works deeply so that we still get ints from our nested lists.
+    #
+    # ([Z] ...)                # @a Z @b produces @a[0] @b[0] @a[1] ...,
+    #                          # In [Z], we end up with ( (1, 4, 7, ...), (2, 5, 8, ...), (3, 6, 9, ...))
+    # flat([Z] ...)            # flatten so that our sub "-> $a, $b, $c { ... }" can pull off three at a time
+
+    for flat([Z] $file.IO.lines».comb(/\d+/)».Int) -> $a, $b, $c {
         my @num = sort($a, $b, $c);
         $possible++ if @num[0] + @num[1] > @num[2];
     }
     say $possible;
+}
+
+multi MAIN('golf', $file="03.in") {
+    say "rows: ", [+] $file.IO.lines.map:{my \n=.comb(/\d+/)».Int.sort;n[0]+n[1]>n[2]};
+
+    # flat([Z] ...)            # as above, since flattened we have ( 1, 4, 7, ..., 2, 5, 8, ..., 3, 6, 9, ... )
+    #    .rotor(3)             # group three-at-a-time ( (1, 4, 7), ... )
+    #    ».sort                # sort each group as integers
+    #    .map:{.[0]+.[1]>.[2]} # return True or False for each. Note: postcircumfix:<[ ]> is just a method so
+    #                          #    we save chars by changing $_[0] to .[0] (which is just $_.[0])
+    # [+] ...                  # just sum up the list, True|False get converted to 1|0
+    say "cols: ", [+] flat([Z] $file.IO.lines».comb(/\d+/)».Int).rotor(3)».sort.map:{.[0]+.[1]>.[2]};
 }
