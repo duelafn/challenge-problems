fec7d6cf67c568597e5e32e161a89d8989f16ad3
Dean Serenevy <dean@serenevy.net>
2020-12-11 08:57:01 -0500

    Day 11, HashMap -> Vec for 500ms -> 67ms

--- 2020/advent-of-code/src/11.rs
+++ 2020/advent-of-code/src/11.rs
@@ -1,8 +1,5 @@
 // SPDX-License-Identifier: MIT
 
-use std::collections::HashMap;
-// use std::convert::TryFrom;
-
 use clap::{Arg, App};
 
 static DIRECTIONS : [(i64,i64); 8] = [
@@ -11,49 +8,13 @@ static DIRECTIONS : [(i64,i64); 8] = [
     (-1, 1), ( 0, 1), ( 1, 1),
 ];
 
-#[derive(Copy, Clone)]
-pub struct BBox(i64,i64,i64,i64);// xmin, xmax, ymin, ymax
-impl BBox {
-    pub fn new() -> BBox {
-        BBox(0,0,0,0)
-    }
-
-    #[inline]
-    pub fn xmin(&self) -> i64 { self.0 }
-    #[inline]
-    pub fn xmax(&self) -> i64 { self.1 }
-    #[inline]
-    pub fn ymin(&self) -> i64 { self.2 }
-    #[inline]
-    pub fn ymax(&self) -> i64 { self.3 }
-
-    #[inline]
-    pub fn in_bounds(&self, x: i64, y: i64) -> bool {
-        x >= self.0 && x <= self.1 && y >= self.2 && y <= self.3
-    }
-
-    #[inline]
-    pub fn update(&mut self, x: i64, y: i64) {
-        if x < self.0 { self.0 = x; }
-        if x > self.1 { self.1 = x; }
-        if y < self.2 { self.2 = y; }
-        if y > self.3 { self.3 = y; }
-    }
-}
-impl std::fmt::Display for BBox {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "BBox: {}, {}, {}, {}", self.0, self.1, self.2, self.3)
-    }
-}
-
 #[derive(Clone)]
 pub struct Floorplan {
-    pub map: HashMap<(i64, i64), char>,
-    pub bbox: BBox,
+    pub map: Vec<Vec<char>>,
 }
 impl Floorplan {
     pub fn new() -> Floorplan {
-        Floorplan { map: HashMap::new(), bbox: BBox::new() }
+        Floorplan { map: Vec::new() }
     }
 
     pub fn load(fname: &str) -> Floorplan {
@@ -61,13 +22,8 @@ impl Floorplan {
         let contents = std::fs::read_to_string(fname)
             .unwrap_or_else(|err| panic!("Error reading {}: {}", fname, err));
 
-        for (j, line) in contents.trim_end_matches('\n').split('\n').enumerate() {
-            for (i, ch) in line.chars().enumerate() {
-                if ch != '.' {
-                    plan.insert((i as i64, j as i64), ch);
-                }
-                plan.bbox.update(i as i64, j as i64);
-            }
+        for line in contents.trim_end_matches('\n').split('\n') {
+            plan.map.push(line.chars().collect());
         }
         return plan;
     }
@@ -76,9 +32,9 @@ impl Floorplan {
         let mut add = Vec::new();
         let mut rm  = Vec::new();
 
-        for y in self.bbox.ymin()..=self.bbox.ymax() {
-            for x in self.bbox.xmin()..=self.bbox.xmax() {
-                match (self.at(x, y), self.count_neighbors(x, y)) {
+        for y in 0..self.map.len() {
+            for x in 0..self.map[y].len() {
+                match (self.map[y][x], self.count_neighbors(x, y)) {
                     ('L', 0)           => { add.push((x,y)); },
                     ('#', n) if n >= 4 => {  rm.push((x,y)); },
                     _ => { },
@@ -88,8 +44,8 @@ impl Floorplan {
 
         if add.is_empty() && rm.is_empty() { return false; }
 
-        for (i, j) in add { self.insert((i, j), '#'); }
-        for (i, j) in rm  { self.insert((i, j), 'L'); }
+        for (i, j) in add { self.map[j][i] = '#'; }
+        for (i, j) in rm  { self.map[j][i] = 'L'; }
         return true;
     }
 
@@ -97,9 +53,9 @@ impl Floorplan {
         let mut add = Vec::new();
         let mut rm  = Vec::new();
 
-        for y in self.bbox.ymin()..=self.bbox.ymax() {
-            for x in self.bbox.xmin()..=self.bbox.xmax() {
-                match (self.at(x, y), self.count_visible(x, y)) {
+        for y in 0..self.map.len() {
+            for x in 0..self.map[y].len() {
+                match (self.map[y][x], self.count_visible(x, y)) {
                     ('L', 0)           => { add.push((x,y)); },
                     ('#', n) if n >= 5 => {  rm.push((x,y)); },
                     _ => { },
@@ -109,27 +65,33 @@ impl Floorplan {
 
         if add.is_empty() && rm.is_empty() { return false; }
 
-        for (i, j) in add { self.insert((i, j), '#'); }
-        for (i, j) in rm  { self.insert((i, j), 'L'); }
+        for (i, j) in add { self.map[j][i] = '#'; }
+        for (i, j) in rm  { self.map[j][i] = 'L'; }
         return true;
     }
 
     #[inline]
-    pub fn count_neighbors(&self, x: i64, y: i64) -> u8 {
+    pub fn count_neighbors(&self, x: usize, y: usize) -> u8 {
         let mut seated = 0;
         for (dx, dy) in DIRECTIONS.iter() {
-            if '#' == self.at(x+dx, y+dy) { seated += 1; }
+            let (x, y) = ((x as i64+dx) as usize, (y as i64+dy) as usize);
+            if y < self.map.len() {
+                let row = &self.map[y];
+                if x < row.len() && '#' == row[x] { seated += 1; }
+            }
         }
         return seated;
     }
 
     #[inline]
-    pub fn count_visible(&self, x: i64, y: i64) -> u8 {
+    pub fn count_visible(&self, x: usize, y: usize) -> u8 {
         let mut seated = 0;
+        let ymax = self.map.len() as i64;
+        let xmax = self.map[0].len() as i64;
         for (dx, dy) in DIRECTIONS.iter() {
-            let (mut x, mut y) = (x+dx, y+dy);
-            while self.bbox.in_bounds(x, y) {
-                match self.at(x, y) {
+            let (mut x, mut y) = (x as i64+dx, y as i64+dy);
+            while x >= 0 && y >= 0 && x < xmax && y < ymax {
+                match self.map[y as usize][x as usize] {
                     '#' => { seated += 1; break; },
                     'L' => { break; },
                     _   => { },
@@ -143,37 +105,19 @@ impl Floorplan {
     #[inline]
     pub fn count(&self, ch: &char) -> u64 {
         let mut n = 0;
-        for y in self.bbox.ymin()..=self.bbox.ymax() {
-            for x in self.bbox.xmin()..=self.bbox.xmax() {
-                match self.map.get(&(x,y)) {
-                    Some(x) if x == ch => { n += 1; },
-                    _ => { },
-                }
+        for row in self.map.iter() {
+            for c in row.iter() {
+                if c == ch { n += 1; }
             }
         }
         return n;
     }
-
-    #[inline]
-    pub fn at(&self, x: i64, y: i64) -> char {
-        match self.map.get(&(x,y)) {
-            Some(x) => *x,
-            None    => '.',
-        }
-    }
-}
-impl std::ops::Deref for Floorplan {
-    type Target = HashMap<(i64, i64), char>;
-    fn deref(&self) -> &Self::Target { &self.map }
-}
-impl std::ops::DerefMut for Floorplan {
-    fn deref_mut(&mut self) -> &mut Self::Target { &mut self.map }
 }
 impl std::fmt::Display for Floorplan {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for y in self.bbox.ymin()..=self.bbox.ymax() {
-            for x in self.bbox.xmin()..=self.bbox.xmax() {
-                write!(f, "{}", self.at(x, y))?;
+        for row in self.map.iter() {
+            for c in row.iter() {
+                write!(f, "{}", c)?;
             }
             write!(f, "{}", "\n")?;
         }
@@ -220,4 +164,3 @@ mod tests {
         assert_eq!(plan.count(&'#'), 2257);
     }
 }
-
