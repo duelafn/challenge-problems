1c9355737a7c728ecaa994eb92910b010bdb8d69
Dean Serenevy <dean@serenevy.net>
2020-12-23 12:06:56 -0500

    2020, Day 23

---
+++ 2020/advent-of-code/src/23.rs
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: MIT
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct ShellGame {
+    vec: Vec<usize>,
+    cur: usize,
+}
+impl ShellGame {
+    pub fn new(init: &[usize], max: usize) -> ShellGame {
+        let mut vec = Vec::with_capacity(max+1);
+        vec.resize(init.len()+1, 0);
+        let first = init[0];
+        let mut last = first;
+        for i in 1..init.len() {
+            vec[last] = init[i];
+            last = init[i];
+        }
+        if max > init.len() {
+            vec[last] = init.len()+1;
+            for i in init.len()+2..=max {
+                vec.push(i)
+            }
+            vec.push(first);
+        }
+        else {
+            vec[last] = first;
+        }
+        ShellGame { vec, cur: first }
+    }
+
+    pub fn shuffle(&mut self) {
+        let old = self.cur;
+        let a = self.vec[old];
+        let b = self.vec[a];
+        let c = self.vec[b];
+        let mut insert = old;
+        loop {
+            insert = if insert == 1 { self.vec.len()-1 } else { insert - 1 };
+            if insert != a && insert != b && insert != c { break; }
+        }
+        self.cur = self.vec[c];
+        self.vec[c] = self.vec[insert];
+        self.vec[insert] = a;
+        self.vec[old] = self.cur;
+    }
+
+    pub fn ident(&self) -> String {
+        let mut rv = String::with_capacity(8);
+        let mut idx = 1;
+        for _ in 1..9 {
+            idx = self.vec[idx];
+            rv.push(std::char::from_digit(idx as u32, 36).unwrap());
+        }
+        return rv;
+    }
+
+    pub fn hash(&self) -> usize { self.vec[1] * self.vec[self.vec[1]] }
+}
+
+impl std::fmt::Display for ShellGame {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut idx = self.cur;
+        for _ in 1..self.vec.len() {
+            write!(f, "{},", idx)?;
+            idx = self.vec[idx];
+        }
+        Ok(())
+    }
+}
+
+
+
+fn main() {
+    let mut game = ShellGame::new(&[3,8,9,5,4,7,6,1,2], 9);
+    for _ in 1..=100 {
+        game.shuffle();
+    }
+    println!("Part 1: identifier {}", game.ident());
+
+    let mut game = ShellGame::new(&[3,8,9,5,4,7,6,1,2], 1_000_000);
+    for _ in 1..=10_000_000 {
+        game.shuffle();
+    }
+    println!("Part 2: hash {}", game.hash());
+}
